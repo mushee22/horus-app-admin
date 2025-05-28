bc0aeb4316437717a2dd950069f4783f5c41b6b2
mushee22 <mmtmusheer@gmail.com>
2025-05-28 07:28:07 +0530

    Update ALLOWED_HOSTS to use the correct production IP address

--- horus/settings.py
+++ horus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-0@6qqoql7s84(z))u%0mk0rz(0aj(n@-+-^_2r#!00q=b=1e4p
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
-    "13.61.190.64"
+    "13.201.128.86"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
